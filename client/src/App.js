d15e324c37d26bbe6c19e1c27db5e2c32ecc387a
abi-v-j <abijoy611@gmail.com>
2023-12-19 18:58:17 +0530

    server side

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState('');
+  const messagesEndRef = useRef(null);
 
-export default App
+  useEffect(() => {
+    // Scroll to the latest message when messages are updated
+    scrollToBottom();
+  }, [messages]);
+
+  const sendMessage = () => {
+    if (inputMessage.trim() !== '') {
+      setMessages([...messages, { text: inputMessage, sender: 'user', timestamp: new Date() }]);
+      setInputMessage('');
+    }
+  };
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        {messages.map((message, index) => (
+          <div key={index} className={`message ${message.sender}`}>
+            <div className="message-text">{message.text}</div>
+            <div className="message-timestamp">{formatTimestamp(message.timestamp)}</div>
+          </div>
+        ))}
+        <div ref={messagesEndRef} />
+      </div>
+      <div className="input-area">
+        <input
+          type="text"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+          placeholder="Type a message..."
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+const formatTimestamp = (timestamp) => {
+  const options = { hour: 'numeric', minute: 'numeric' };
+  return new Intl.DateTimeFormat('en-US', options).format(timestamp);
+};
+
+export default App;
--- server/Server.js
+++ server/Server.js
@@ -1,1 +1,22 @@
-console.log('hi');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require('./config/DB')
+
+const port = 7000
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+connectDB()
+
+
+
+
+
+app.listen(port, () => {
+    console.log("Server is Running");
+  });
+  
