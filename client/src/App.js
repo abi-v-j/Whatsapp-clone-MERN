e08657211ab9164718b941576e214f220c0bd222
abi-v-j <abijoy611@gmail.com>
2024-01-03 18:38:23 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,25 @@
-import React, { useState } from 'react';
-import { Box, Paper, TextField, Button, Typography } from '@mui/material';
+import logo from './logo.svg';
+import './App.css';
 
-const ChatPage = () => {
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState('');
-
-  const handleSendMessage = () => {
-    if (newMessage.trim() !== '') {
-      setMessages([...messages, { text: newMessage }]);
-      setNewMessage('');
-    }
-  };
-
+function App() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        '& > :not(style)': {
-          m: 1,
-          width: '400px',
-          height: '620px',
-        },
-      }}
-    >
-      <Paper elevation={3} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Paper elevation={3} sx={{ width: '100%', height: '90%', overflowY: 'auto', padding: '10px' }}>
-          {messages.map((message, index) => (
-            <Typography key={index} variant="body1" gutterBottom>
-              {message.text}
-            </Typography>
-          ))}
-        </Paper>
-        <Box sx={{ width: '70%', mt: 2 }}>
-          <TextField
-            id="filled-basic"
-            label="Type a message"
-            variant="filled"
-            fullWidth
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-          />
-        </Box>
-        <Button variant="contained" color="primary" onClick={handleSendMessage} sx={{ mt: 2 }}>
-          Send
-        </Button>
-      </Paper>
-    </Box>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
-export default ChatPage;
+export default App;
